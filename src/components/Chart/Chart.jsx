1354b70f465a10b76d39443941f26650ed053835
Skadi <kartalov.pt@gmail.com>
2020-04-21 18:48:47 +0200

    Covid19 Tracker complete

--- src/components/Chart/Chart.jsx
+++ src/components/Chart/Chart.jsx
@@ -4,8 +4,8 @@ import { Line, Bar } from 'react-chartjs-2';
 
 import styles from './Chart.module.css';
 
-const Chart = () => {
-  const [dailyData, setDailyData] = useState({});
+const Chart = ({ data: { confirmed, deaths, recovered }, country }) => {
+  const [dailyData, setDailyData] = useState([]);
 
   useEffect(() => {
     const fetchAPI = async () => {
@@ -13,21 +13,56 @@ const Chart = () => {
     };
 
     fetchAPI(); // We can self invoke it or use it like this.
-  });
+  }, []);
 
-  const lineChart = dailyData[0] ? (
+  const lineChart = dailyData.length ? ( // Same as saying dailyData.length !== 0 ?. Because 0 length of array returns falsy, and any real number returns truthy.
     <Line
       data={{
-        labels: '',
-        datasets: [{}, {}]
+        labels: dailyData.map(({ date }) => date), // This is a map
+        datasets: [
+          {
+            data: dailyData.map(({ confirmed }) => confirmed),
+            label: 'Infected',
+            borderColor: '#3333ff',
+            fill: true
+          },
+          {
+            data: dailyData.map(({ deaths }) => deaths),
+            label: 'Deaths',
+            borderColor: 'red',
+            backgroundColor: 'rgba(255, 0, 0, 0.5)',
+            fill: true
+          }
+        ]
+      }}
+    />
+  ) : null;
+
+  const barChart = confirmed ? (
+    <Bar
+      data={{
+        labels: ['Infected', 'Recovered', 'Deaths'],
+        datasets: [
+          {
+            label: 'People',
+            backgroundColor: [
+              'rgba(0, 0, 255, 0.5)',
+              'rgba(0, 255, 0, 0.5)',
+              'rgba(255, 0, 0, 0.5)'
+            ],
+            data: [confirmed.value, recovered.value, deaths.value]
+          }
+        ]
+      }}
+      options={{
+        legend: { display: false },
+        title: { display: true, text: `Current state in ${country}` }
       }}
     />
   ) : null;
 
   return (
-    <div>
-      <h1>Chart</h1>
-    </div>
+    <div className={styles.container}>{country ? barChart : lineChart}</div>
   );
 };
 
